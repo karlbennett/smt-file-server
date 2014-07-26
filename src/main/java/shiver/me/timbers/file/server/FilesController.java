22df3dfa532b31b85e7c0b3d431ddb67db015444
karlbennett <where_1ts@yahoo.com>
2014-07-26 22:42:17 +1200

    Formatted code.

--- src/main/java/shiver/me/timbers/file/server/FilesController.java
+++ src/main/java/shiver/me/timbers/file/server/FilesController.java
@@ -3,21 +3,17 @@ package shiver.me.timbers.file.server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import shiver.me.timbers.file.io.Directory;
 import shiver.me.timbers.file.io.InvalidPathException;
 import shiver.me.timbers.file.io.JavaDirectory;
-import shiver.me.timbers.file.io.JavaFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.util.concurrent.Callable;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
