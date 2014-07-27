544e5ab124dafca8f6d0a0551559da49ca28532d
karlbennett <where_1ts@yahoo.com>
2014-07-27 16:40:10 +1200

    Updated the FilesController so that it better inspects the served files mime type. Also added integration tests.

--- src/main/java/shiver/me/timbers/file/server/FilesController.java
+++ src/main/java/shiver/me/timbers/file/server/FilesController.java
@@ -1,6 +1,9 @@
 package shiver.me.timbers.file.server;
 
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +14,10 @@ import shiver.me.timbers.file.io.JavaDirectory;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -42,8 +48,19 @@ public class FilesController {
     }
 
     @RequestMapping(value = "/file", method = GET)
-    public FileSystemResource file(@ModelAttribute String path) throws IOException {
+    public ResponseEntity<FileSystemResource> file(@ModelAttribute String path) throws IOException {
 
-        return new FileSystemResource(new File(path));
+        final HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(inspectMediaType(path));
+
+        return new ResponseEntity<>(new FileSystemResource(new File(path)), headers, OK);
+    }
+
+    private static MediaType inspectMediaType(String path) throws IOException {
+
+        final String mimeType = Files.probeContentType(Paths.get(path));
+
+        return MediaType.valueOf(mimeType);
     }
 }
---
+++ src/test/java/shiver/me/timbers/file/server/FilesControllerTest.java
@@ -0,0 +1,80 @@
+package shiver.me.timbers.file.server;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static shiver.me.timbers.Constants.DIRECTORY_FOUR_MODIFICATION_DATE;
+import static shiver.me.timbers.Constants.DIRECTORY_ONE_NAME;
+import static shiver.me.timbers.Constants.DIRECTORY_ONE_PATH;
+import static shiver.me.timbers.Constants.FILE_ONE_PATH;
+import static shiver.me.timbers.Constants.FILE_ONE_TEXT;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = FilesConfiguration.class)
+@WebAppConfiguration("classpath:")
+public class FilesControllerTest {
+
+    private static final String ERROR_MESSAGE = "No path provided.";
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void I_can_request_a_directory() throws Exception {
+
+        mockMvc.perform(get("/directory").requestAttr("path", DIRECTORY_ONE_PATH).accept(APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.name").value(DIRECTORY_ONE_NAME))
+                .andExpect(jsonPath("$.extension").value(""))
+                .andExpect(jsonPath("$.modified").value(DIRECTORY_FOUR_MODIFICATION_DATE.getTime()));
+    }
+
+    @Test
+    public void I_cannot_request_a_directory_without_a_path() throws Exception {
+
+        mockMvc.perform(get("/directory"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.error").value(ERROR_MESSAGE));
+    }
+
+    @Test
+    public void I_can_request_a_file() throws Exception {
+
+        mockMvc.perform(get("/file").requestAttr("path", FILE_ONE_PATH))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(TEXT_PLAIN))
+                .andExpect(content().string(FILE_ONE_TEXT));
+    }
+
+    @Test
+    public void I_cannot_request_a_file_without_a_path() throws Exception {
+
+        mockMvc.perform(get("/file"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.error").value(ERROR_MESSAGE));
+    }
+}
